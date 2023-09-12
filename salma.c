f55abc45d383b794862502e618fa619a0ec8d8f5
salmaelgadi <elgadi.salma0@gmail.com>
2023-09-12 16:52:29 +0100

    test

---
+++ salma.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+float a,b,d;
+printf("my first programe \n");
+printf("ecrire a\t");
+scanf("%f",&a);
+printf("ecrire b\t");
+scanf("%f",&b);
+d=a+b;
+printf("la somme est : %.2f",d);
+}
