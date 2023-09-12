7ab82ba1729c4c2a3b721e7c97ee31b8dc4d0928
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2023-09-12 14:16:08 +0100

    Add files via upload

---
+++ yassine.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main(void){
+int a,b;
+char oper;
+printf("Donner la valeur du premier nombre\t");
+scanf("%d",&a);
+printf("Donner le valeur de deuxieme nombre\t");
+scanf("%d",&b);
+printf("Donner l operation a utiliser\t");
+scanf(" %c",&oper);
+switch(oper){
+case '+':printf("%d + %d = %d",a,b,a+b);
+break;
+case '-': printf("%d -%d = %d",a,b,a-b);
+break;
+case '*': printf("%d * %d =%d",a,b,a*b);
+break;
+case '/' :if(b==0){
+printf("math erreur");
+}else{
+printf("%d / %d = %d",a,b,a/b);
+}
+break;
+case '%' : printf("%d modulo  %d = %d",a,b,a%b);
+break;
+default : printf("operation non disponible");
+}
+}
