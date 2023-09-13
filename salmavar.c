89135c357b751b76a33b064d987873791fd88b17
Elgadisalma <elgadi.salma0@gmail.com>
2023-09-13 17:30:45 +0100

    salma

---
+++ salmavar.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+char  nom[]="salma";
+int age=12;
+float prix=15.5;
+bool prop = true;
+printf("%d\n",age);
+printf("%s\n",nom);
+printf("%f\n",prix);
+if (prop){
+printf("la variable est vraie (true).\n");
+}else{
+printf("la variable est fausse (false).\n");
+}
+
+
+
+
+
+
+}
