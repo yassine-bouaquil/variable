db9104798687af2364ebf94abd64cfa33a84db0b
Rochdi <abada.rochdiv@gmail.com>
2023-09-13 18:12:41 +0200

    rochdi

---
+++ rochdi.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+int age=22;
+float poid=77.5;
+char nom[]="rochdi";
+char prenom[]="abada";
+bool est_homme=true;
+printf("\nnom=%s  ,prenom=%s  \n",nom,prenom);
+printf("age=%d  ,poid=%.2f",age,poid);
+if(est_homme)
+printf("true");
+else
+printf("false");
+
+
+
+
+
+
+
+
+
+}
